a1dc99abb8e2c47a8a83fc2430f8c4463f01ad40
Henrique Cardoso de Faria <elo.henrique@gmail.com>
2015-06-25 19:47:57 -0300

    Add user model.

--- app/models/invoice.js
+++ app/models/invoice.js
@@ -4,5 +4,6 @@ export default DS.Model.extend({
   category: DS.attr('string'),
   value: DS.attr('number'),
   group: DS.belongsTo('group'),
+  user: DS.belongsTo('user'),
   date: DS.attr("string")
 });
