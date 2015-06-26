b3273893d44e716166c1999b921b8a92ed859758
Henrique Cardoso de Faria <elo.henrique@gmail.com>
2015-06-25 22:38:44 -0300

    Refactor invoices-list more.

--- app/components/invoices-list/component.js
+++ app/components/invoices-list/component.js
@@ -3,7 +3,7 @@ import Ember from 'ember';
 export default Ember.Component.extend({
   invoicesList: Ember.computed('list', function() {
     var listSize = this.get('list-size');
-    var list = this.get('list').toArray();
+    var list = this.get('list');
     if(listSize) {
       list = list.slice(0, listSize);
     }
