4b7caa5702b7aef4a64ef5efa8a6d2dc1fec6b02
Henrique Cardoso de Faria <elo.henrique@gmail.com>
2015-06-26 20:43:03 -0300

    Set balance in total.

--- app/components/total-value/component.js
+++ app/components/total-value/component.js
@@ -4,13 +4,20 @@ export default Ember.Component.extend({
   expenses: Ember.computed.filterBy('list', 'revenue', false),
   revenue: Ember.computed.filterBy('list', 'revenue', true),
   
-  expenses_values: Ember.computed.map('expenses', function(invoice) {
+  expensesValues: Ember.computed.map('expenses', function(invoice) {
     return Number(invoice.get('value'));
   }),
-  revenue_values: Ember.computed.map('revenue', function(invoice) {
+  revenueValues: Ember.computed.map('revenue', function(invoice) {
     return Number(invoice.get('value'));
   }),
   
-  expenses_total: Ember.computed.sum('expenses_values'),
-  revenue_total: Ember.computed.sum('revenue_values')
+  expensesTotal: Ember.computed.sum('expensesValues'),
+  revenueTotal: Ember.computed.sum('revenueValues'),
+  
+  balance: Ember.computed('expensesTotal', 'revenueTotal', function() {
+    return (this.get('revenueTotal') - this.get('expensesTotal'));
+  }),
+  isBalanceRevenue: Ember.computed('expensesTotal', 'revenueTotal', function() {
+    return (this.get('revenueTotal') > this.get('expensesTotal'));
+  })
 });
