eb5df303c42c565653485fa0d58379248b94a15b
Henrique Cardoso de Faria <elo.henrique@gmail.com>
2015-06-25 22:14:18 -0300

    Refactored function that calculates sum of values.

--- app/components/total-value/component.js
+++ app/components/total-value/component.js
@@ -1,12 +1,9 @@
 import Ember from 'ember';
 
 export default Ember.Component.extend({
-  total: function() {
-    var invoicesList = this.get('list').toArray();
-    console.log(invoicesList[0]);
-    console.log(invoicesList[0].value);
-    return invoicesList.reduce(function(aggregator, invoice) {
-      return aggregator + invoice.get('value');
-    }, 0);
-  }.property('list.@each.value')
+  values: Ember.computed.map('list', function(invoice, index) {
+    return invoice.get('value');
+  }),
+  
+  total: Ember.computed.sum('values')
 });
