93733e400d645d8234bd1ab65ad22173bca98abc
Henrique Cardoso de Faria <elo.henrique@gmail.com>
2015-06-26 13:44:24 -0300

    Converting to number to guarantee it will sum correctly.

--- app/components/total-value/component.js
+++ app/components/total-value/component.js
@@ -2,7 +2,7 @@ import Ember from 'ember';
 
 export default Ember.Component.extend({
   values: Ember.computed.map('list', function(invoice, index) {
-    return invoice.get('value');
+    return Number(invoice.get('value'));
   }),
   
   total: Ember.computed.sum('values')
