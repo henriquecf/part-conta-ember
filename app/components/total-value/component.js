6cc7f8b3f700ecb267c2953f565d3b5acd958f0b
Henrique Cardoso de Faria <elo.henrique@gmail.com>
2015-06-29 15:46:30 -0300

    Add tests for total-value component.

--- app/components/total-value/component.js
+++ app/components/total-value/component.js
@@ -1,21 +1,21 @@
 import Ember from 'ember';
 
 export default Ember.Component.extend({
-  expenses: Ember.computed.filterBy('list', 'revenue', false),
-  revenue: Ember.computed.filterBy('list', 'revenue', true),
+  _expenses: Ember.computed.filterBy('list', 'revenue', false),
+  _revenue: Ember.computed.filterBy('list', 'revenue', true),
   
-  expensesValues: Ember.computed.map('expenses', function(invoice) {
+  _expensesValues: Ember.computed.map('_expenses', function(invoice) {
     return Number(invoice.get('value'));
   }),
-  revenueValues: Ember.computed.map('revenue', function(invoice) {
+  _revenueValues: Ember.computed.map('_revenue', function(invoice) {
     return Number(invoice.get('value'));
   }),
   
-  expensesTotal: Ember.computed.sum('expensesValues'),
-  revenueTotal: Ember.computed.sum('revenueValues'),
+  expensesTotal: Ember.computed.sum('_expensesValues'),
+  revenueTotal: Ember.computed.sum('_revenueValues'),
   
   balance: Ember.computed('expensesTotal', 'revenueTotal', function() {
-    return (this.get('revenueTotal') - this.get('expensesTotal'));
+    return Math.abs((this.get('revenueTotal') - this.get('expensesTotal')));
   }),
   isBalanceRevenue: Ember.computed('expensesTotal', 'revenueTotal', function() {
     return (this.get('revenueTotal') > this.get('expensesTotal'));
--- tests/unit/components/total-value/component-test.js
+++ tests/unit/components/total-value/component-test.js
@@ -1,4 +1,5 @@
 import { moduleForComponent, test } from 'ember-qunit';
+import Ember from 'ember';
 
 moduleForComponent('total-value', 'Unit | Component | total value', {
   // Specify the other units that are required for this test
@@ -7,13 +8,32 @@ moduleForComponent('total-value', 'Unit | Component | total value', {
 });
 
 test('it renders', function(assert) {
-  assert.expect(2);
+  assert.expect(9);
 
   // Creates the component instance
   var component = this.subject();
   assert.equal(component._state, 'preRender');
+  
+  var list = [
+    Ember.Object.create({value: 20, revenue: false}),
+    Ember.Object.create({value: 30, revenue: true}),
+    Ember.Object.create({value: 100, revenue: false}),
+    Ember.Object.create({value: 95, revenue: true})
+  ];
 
-  // Renders the component to the page
-  this.render();
-  assert.equal(component._state, 'inDOM');
+  Ember.run(function() {
+    component.set('list', list);
+  
+    assert.equal(component.get('revenueTotal'), 125);
+    assert.equal(component.get('expensesTotal'), 120);
+    assert.equal(component.get('balance'), 5);
+    assert.equal(component.get('isBalanceRevenue'), true);
+  
+    list.pushObject(Ember.Object.create({value: 21, revenue: false}));
+  
+    assert.equal(component.get('revenueTotal'), 125);
+    assert.equal(component.get('expensesTotal'), 141);
+    assert.equal(component.get('balance'), 16);
+    assert.equal(component.get('isBalanceRevenue'), false);
+  });
 });
