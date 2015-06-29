2074b710a062e4c151ff229ccb3562fb34c4f734
Henrique Cardoso de Faria <elo.henrique@gmail.com>
2015-06-29 14:26:52 -0300

    Implement tests for invoices-list component.

--- tests/unit/components/invoices-list/component-test.js
+++ tests/unit/components/invoices-list/component-test.js
@@ -1,19 +1,29 @@
 import { moduleForComponent, test } from 'ember-qunit';
+import Ember from 'ember';
 
 moduleForComponent('invoices-list', 'Unit | Component | invoices list', {
   // Specify the other units that are required for this test
-  // needs: ['component:foo', 'helper:bar'],
+  needs: ['component:number-to-currency'],
   unit: true
 });
 
 test('it renders', function(assert) {
-  assert.expect(2);
+  assert.expect(3);
 
   // Creates the component instance
   var component = this.subject();
   assert.equal(component._state, 'preRender');
 
-  // Renders the component to the page
-  this.render();
-  assert.equal(component._state, 'inDOM');
+  Ember.run(function() {
+    component.set('list', [{},{},{},{},{},{}]);
+  });
+  
+  assert.equal(this.$().find('hr').length, 6);
+  
+  // Should limit the number of lists when set in listSize
+  Ember.run(function() {
+    component.set('list-size', 3);
+  });
+  
+  assert.equal(this.$().find('hr').length, 3);
 });
