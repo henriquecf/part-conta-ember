6e8579680c43e504a0740197073549a2fd1b8900
Henrique Cardoso de Faria <elo.henrique@gmail.com>
2015-06-29 22:54:53 -0300

    Remove unused var.

--- app/new-invoice/route.js
+++ app/new-invoice/route.js
@@ -3,7 +3,7 @@ import Ember from 'ember';
 export default Ember.Route.extend({
   actions: {
     addInvoice: function(invoice) {
-      var invoice = this.store.createRecord('invoice', invoice);
+      this.store.createRecord('invoice', invoice);
       this.transitionTo('invoices');
     }
   }
