8f8cd4c6a0f73609586163f7980f039635e89841
Henrique Cardoso de Faria <elo.henrique@gmail.com>
2015-07-23 19:08:18 -0300

    Add AuthenticatedRouteMixin to invoices and new-invoice routes.

--- app/new-invoice/route.js
+++ app/new-invoice/route.js
@@ -1,6 +1,7 @@
 import Ember from 'ember';
+import AuthenticatedRouteMixin from 'simple-auth/mixins/authenticated-route-mixin';
 
-export default Ember.Route.extend({
+export default Ember.Route.extend(AuthenticatedRouteMixin, {
   actions: {
     addInvoice: function(invoice) {
       this.store.createRecord('invoice', invoice);
