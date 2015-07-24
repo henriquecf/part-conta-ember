354e5a96038f9a3021419e5afcbde491c020703a
Henrique Cardoso de Faria <elo.henrique@gmail.com>
2015-07-24 13:14:16 -0300

    Add standalone version of ActiveModelAdapter.

--- app/adapters/application.js
+++ app/adapters/application.js
@@ -1,6 +1,6 @@
-import DS from 'ember-data';
+import ActiveModelAdapter from 'active-model-adapter';
 
-export default DS.ActiveModelAdapter.extend({
+export default ActiveModelAdapter.extend({
   namespace: 'api',
   // Uncomment the following lines when using the local API
   host: 'http://localhost:3000',
