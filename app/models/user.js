91e1acf5c27affd361f1f29098436a79da164d7c
Henrique Cardoso de Faria <elo.henrique@gmail.com>
2015-07-28 23:50:05 -0300

    Add groups to user.

--- app/models/user.js
+++ app/models/user.js
@@ -3,5 +3,6 @@ import DS from 'ember-data';
 export default DS.Model.extend({
   name: DS.attr('string'),
   image: DS.attr('string'),
-  email: DS.attr('string')
+  email: DS.attr('string'),
+  groups: DS.hasMany('group')
 });
