a106f2c96404ba3d42e7a8b7027fef078a1d4316
Henrique Cardoso de Faria <elo.henrique@gmail.com>
2015-07-29 15:29:27 -0300

    Set users and groups for user model.

--- app/models/user.js
+++ app/models/user.js
@@ -1,8 +1,16 @@
 import DS from 'ember-data';
+import Ember from 'ember';
 
 export default DS.Model.extend({
   name: DS.attr('string'),
   image: DS.attr('string'),
   email: DS.attr('string'),
-  groups: DS.hasMany('group')
+  groups: DS.hasMany('group'),
+  users: DS.hasMany('user'),
+  
+  allUsers: Ember.computed('users', function() {
+    var usersList = this.get('users').slice();
+    usersList.splice(0, 0, this);
+    return usersList;
+  })
 });
--- tests/unit/models/user-test.js
+++ tests/unit/models/user-test.js
@@ -1,12 +1,18 @@
 import { moduleForModel, test } from 'ember-qunit';
+import Ember from 'ember';
 
 moduleForModel('user', 'Unit | Model | user', {
   // Specify the other units that are required for this test.
-  needs: ['model:invoice']
+  needs: ['model:invoice', 'model:group']
 });
 
-test('it exists', function(assert) {
+test('it returns friends with himself', function(assert) {
   var model = this.subject();
   // var store = this.store();
-  assert.ok(!!model);
+  var other_user = this.subject();
+  Ember.run(function() {
+    model.set('users', [other_user]);
+  });
+  
+  assert.equal(JSON.stringify(model.get('allUsers')), JSON.stringify([model, other_user]));
 });
