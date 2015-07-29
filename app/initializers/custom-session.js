849050c4631daecbc19f16091f81ce60bf977d84
Henrique Cardoso de Faria <elo.henrique@gmail.com>
2015-07-28 23:44:16 -0300

    Add currentUser on session creation.

---
+++ app/initializers/custom-session.js
@@ -0,0 +1,21 @@
+import Ember from "ember";  
+import Session from "simple-auth/session";
+
+export default {
+  name: "current-user",
+  before: "simple-auth",
+  initialize: function(container) {
+    Session.reopen({
+      setCurrentUser: function() {
+        var accessToken = this.get('secure.access_token');
+        var self = this;
+
+        if (!Ember.isEmpty(accessToken)) {
+          return container.lookup('store:main').find('user', 'me').then(function(user) {
+            self.set('content.currentUser', user);
+          });
+        }
+      }.observes('secure.access_token')
+    });
+  }
+};
