a8e14d7392c51e8af665be7580ee5a93180d9d51
Henrique Cardoso de Faria <elo.henrique@gmail.com>
2015-07-30 18:44:23 -0300

    Add mixin for invoice form logic.

---
+++ app/mixins/invoice-form.js
@@ -0,0 +1,22 @@
+import Ember from 'ember';
+
+export default Ember.Mixin.create({
+  revenue: false,
+  categories: ["Alimentação", "Educação", "Lazer", "Moradia", "Roupas e Acessórios", "Saúde e Beleza", 
+               "Transporte", "Diversos", "Receita"],
+  
+  actions: {
+    addInvoice: function() {
+      var invoiceFields = this.controller.getProperties('value', 'category', 'revenue', 'description', 'date');
+      var invoice = this.store.createRecord('invoice', invoiceFields);
+      invoice.save();
+      this.transitionTo('dashboard');
+    }
+  },
+  
+  setupController: function(controller, model) {
+    this._super(controller, model);
+    controller.set('categories', this.get('categories'));
+    controller.set('revenue', this.get('revenue'));
+  }
+});
--- app/new-invoice/route.js
+++ app/new-invoice/route.js
@@ -1,22 +1,5 @@
 import Ember from 'ember';
 import AuthenticatedRouteMixin from 'simple-auth/mixins/authenticated-route-mixin';
+import InvoiceFormMixin from '../mixins/invoice-form';
 
-export default Ember.Route.extend(AuthenticatedRouteMixin, {
-  revenue: false,
-  categories: ["Alimentação", "Educação", "Lazer", "Moradia", "Roupas e Acessórios", "Saúde e Beleza", 
-               "Transporte", "Diversos", "Receita"],
-  
-  actions: {
-    addInvoice: function() {
-      var invoiceFields = this.controller.getProperties('value', 'category', 'revenue', 'description', 'date');
-      var invoice = this.store.createRecord('invoice', invoiceFields);
-      invoice.save();
-      this.transitionTo('dashboard');
-    }
-  },
-  
-  setupController: function(controller, model) {
-    this._super(controller, model);
-    controller.set('categories', this.get('categories'));
-  }
-});
+export default Ember.Route.extend(AuthenticatedRouteMixin, InvoiceFormMixin);
