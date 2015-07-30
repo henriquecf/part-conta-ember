ddc93772dee85beafa77932ab0ecf875a007e035
Henrique Cardoso de Faria <elo.henrique@gmail.com>
2015-07-30 18:47:19 -0300

    Add partial form to edit invoice.

--- app/edit-invoice/route.js
+++ app/edit-invoice/route.js
@@ -1,7 +1,8 @@
 import Ember from 'ember';
 import AuthenticatedRouteMixin from 'simple-auth/mixins/authenticated-route-mixin';
+import InvoiceFormMixin from '../mixins/invoice-form';
 
-export default Ember.Route.extend(AuthenticatedRouteMixin, {
+export default Ember.Route.extend(AuthenticatedRouteMixin, InvoiceFormMixin, {
   model: function(params) {
     this.store.find('invoice', params.id);
   }
