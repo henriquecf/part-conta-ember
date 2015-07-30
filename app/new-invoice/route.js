cbbb6b03b66f369d568892ca7ed8f6644f85f8d1
Henrique Cardoso de Faria <elo.henrique@gmail.com>
2015-07-30 18:39:33 -0300

    Change invoice form to partial instead of component.

--- app/new-invoice/route.js
+++ app/new-invoice/route.js
@@ -2,11 +2,21 @@ import Ember from 'ember';
 import AuthenticatedRouteMixin from 'simple-auth/mixins/authenticated-route-mixin';
 
 export default Ember.Route.extend(AuthenticatedRouteMixin, {
+  revenue: false,
+  categories: ["Alimentação", "Educação", "Lazer", "Moradia", "Roupas e Acessórios", "Saúde e Beleza", 
+               "Transporte", "Diversos", "Receita"],
+  
   actions: {
-    addInvoice: function(invoiceFields) {
+    addInvoice: function() {
+      var invoiceFields = this.controller.getProperties('value', 'category', 'revenue', 'description', 'date');
       var invoice = this.store.createRecord('invoice', invoiceFields);
       invoice.save();
       this.transitionTo('dashboard');
     }
+  },
+  
+  setupController: function(controller, model) {
+    this._super(controller, model);
+    controller.set('categories', this.get('categories'));
   }
 });
