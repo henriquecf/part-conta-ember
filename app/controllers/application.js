852979b58399642618b9869c00aa587cdeaaf6b7
Henrique Cardoso de Faria <elo.henrique@gmail.com>
2015-07-31 19:49:01 -0300

    Add basic layout for ember-material-design.

---
+++ app/controllers/application.js
@@ -0,0 +1,20 @@
+import Ember from 'ember';
+
+export default Ember.Controller.extend({
+    mediaQueries: Ember.inject.service('media-queries'),
+
+    init: function() {
+        this.get('mediaQueries').match('gt-sm', '(min-width: 600px)');
+        //this.set('sidebarLocked', this.get('mediaQueries.isGtSm'));
+    },
+
+    sidebarLocked: Ember.computed('mediaQueries.isGtSm', function() {
+        return this.get('mediaQueries.isGtSm');
+    }),
+
+    actions: {
+        toggleSidebar: function() {
+            this.toggleProperty('sidebarVisible');
+        }
+    }
+});
