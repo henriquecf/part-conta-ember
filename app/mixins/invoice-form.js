a0de17ac37b6364604ad27f188a1f990cd78a419
Henrique Cardoso de Faria <elo.henrique@gmail.com>
2015-08-02 15:49:13 -0300

    Now able to edit invoices.

--- app/mixins/invoice-form.js
+++ app/mixins/invoice-form.js
@@ -12,22 +12,34 @@ export default Ember.Mixin.create({
       var invoice = this.store.createRecord('invoice', invoiceFields);
       invoice.save();
       this.transitionTo('dashboard');
+    },
+    
+    editInvoice: function() {
+      var ctrl = this.controller;
+      var invoice = ctrl.get('model');
+      invoice.setProperties({value: ctrl.get('value'), category: ctrl.get('category'), date: ctrl.get('date'),
+        revenue: ctrl.get('revenue'), description: ctrl.get('description'), user: ctrl.get('user'), group: ctrl.get('group')});
+      invoice.save();
+      this.transitionTo('dashboard');
     }
   },
-  
+    
   setupController: function(controller, model) {
     this._super(controller, model);
     controller.set('categories', this.get('categories'));
     controller.set('revenue', this.get('revenue'));
+    var addOrEdit = 'addInvoice';
     if(model) {
       controller.setProperties({value: model.get('value'), category: model.get('category'),
                                 revenue: model.get('revenue'), user: model.get('user'),
                                 description: model.get('description'), date: model.get('date'),
                                 group: model.get('group')});
+      addOrEdit = "editInvoice";
     }
     else {
       controller.setProperties({value: null, category: null, revenue: null, date: null, 
                                 description: null, user: null, group: null});
     }
+    controller.set('addOrEdit', addOrEdit);
   }
 });
