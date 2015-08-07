d9827ce5a3b229021988a0271262073988e48c45
Henrique Cardoso de Faria <elo.henrique@gmail.com>
2015-08-07 12:34:19 -0300

    Set duration of transitions.

--- app/transitions.js
+++ app/transitions.js
@@ -1,34 +1,31 @@
 export default function(){
+  var duration = 1000;
+  
   this.transition(
     this.fromRoute('dashboard'),
     this.toRoute('invoices'),
-    this.use('toLeft'),
-    this.reverse('toRight')
+    this.use('toLeft', {duration: duration}),
+    this.reverse('toRight', {duration: duration})
   );
   
   this.transition(
     this.fromRoute('invoices'),
     this.toRoute('reports'),
-    this.use('toLeft'),
-    this.reverse('toRight')
+    this.use('toLeft', {duration: duration}),
+    this.reverse('toRight', {duration: duration})
   );
   
   this.transition(
     this.fromRoute('dashboard'),
     this.toRoute('reports'),
-    this.use('toLeft'),
-    this.reverse('toRight')
+    this.use('toLeft', {duration: duration}),
+    this.reverse('toRight', {duration: duration})
   );
   
   this.transition(
     this.fromRoute('reports'),
     this.toRoute('report'),
-    this.use('toLeft'),
-    this.reverse('toRight')
-  );
-  
-  this.transition(
-    this.hasClass('report-details'),
-    this.use('crossFade')
+    this.use('toLeft', {duration: duration}),
+    this.reverse('toRight', {duration: duration})
   );
 }
