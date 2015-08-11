3dc6456f84c1d24513cc993a63774a5aed096b7f
Henrique Cardoso de Faria <elo.henrique@gmail.com>
2015-08-11 15:49:32 -0300

    Correct tests.

--- app/routes/application.js
+++ app/routes/application.js
@@ -1,6 +1,5 @@
 import Ember from 'ember';
 import ApplicationRouteMixin from 'simple-auth/mixins/application-route-mixin';
-import moment from 'moment';
 
 export default Ember.Route.extend(ApplicationRouteMixin, {
   actions: {
--- app/routes/index.js
+++ app/routes/index.js
@@ -2,7 +2,7 @@ import Ember from 'ember';
 import UnauthenticatedRouteMixin from 'simple-auth/mixins/unauthenticated-route-mixin';
 
 export default Ember.Route.extend(UnauthenticatedRouteMixin, {
-  beforeModel: function(transition) {
+  beforeModel: function() {
     if(this.get('session.isAuthenticated')){
       this.transitionTo('dashboard');
     }
