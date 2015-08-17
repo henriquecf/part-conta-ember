911cb62ff41b96737871bebf22154f092c3f3bd3
Henrique Cardoso de Faria <elo.henrique@gmail.com>
2015-08-17 12:33:58 -0300

    Add names to edit and add routes.

--- app/edit-invoice/route.js
+++ app/edit-invoice/route.js
@@ -3,6 +3,11 @@ import AuthenticatedRouteMixin from 'simple-auth/mixins/authenticated-route-mixi
 import CancelButtonMixin from '../mixins/cancel-button';
 
 export default Ember.Route.extend(AuthenticatedRouteMixin, CancelButtonMixin, {
+  beforeModel: function(transition) {
+    this._super(transition);
+    this.controllerFor('application').set('pageTitle', 'Editar conta');
+  },
+  
   model: function(params) {
     this.store.find('invoice', params.id);
   }
--- app/new-invoice/route.js
+++ app/new-invoice/route.js
@@ -2,4 +2,9 @@ import Ember from 'ember';
 import AuthenticatedRouteMixin from 'simple-auth/mixins/authenticated-route-mixin';
 import CancelButtonMixin from '../mixins/cancel-button';
 
-export default Ember.Route.extend(AuthenticatedRouteMixin, CancelButtonMixin);
+export default Ember.Route.extend(AuthenticatedRouteMixin, CancelButtonMixin, {
+  beforeModel: function(transition) {
+    this._super(transition);
+    this.controllerFor('application').set('pageTitle', 'Nova conta');
+  }
+});
